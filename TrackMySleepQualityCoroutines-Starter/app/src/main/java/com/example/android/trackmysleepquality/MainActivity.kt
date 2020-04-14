ddfdf8a91256b1a2d6d8b0bcec658a57ad2f39c8
khengsian <limks-aa16@student.tarc.edu.my>
2020-04-14 19:55:50 +0800

    first

--- TrackMySleepQualityCoroutines-Starter/app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
+++ TrackMySleepQualityCoroutines-Starter/app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
@@ -31,7 +31,7 @@ import androidx.appcompat.app.AppCompatActivity
  * - Room database, DAO, and Coroutines
  *
  * It also uses and builds on the following techniques from previous lessons:
- * - Transformation map ghgfhfgh
+ * - Transformation map ghgfhfgh dfgfgdfgdfgdfg
  * - Data Binding in XML files
  * - ViewModel Factory
  * - Using Backing Properties to protect MutableLiveData
